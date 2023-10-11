e9930312098aa6decefcc7f8eb1cd4529eac5ab7
LF-108 <113195811+LF-108@users.noreply.github.com>
2023-10-10 22:19:16 -0400

    Add files via upload
    A file that tests the basic header

---
+++ header_test/header_test.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include "../include/myheader.h"
+
+int main()
+{
+program_identification(5, "Day of the Week", "This program calculates the day of the week \n");
+
+return EXIT_SUCCESS;
+}
+
